3dcaa07dc2fedd5183d4f79871d153b656b71bea
msmsandy <sanzoidy@gmail.com>
2021-03-27 17:22:47 -0400

    fame: end

--- commands/fame.js
+++ commands/fame.js
@@ -1,6 +1,6 @@
 const argumentType = {
     view: {
-        command: '', 
+        command: 'view', 
         description: 'view current contest'
     },
     enter : {
@@ -12,18 +12,18 @@ const argumentType = {
         command: 'dropout',
         description: 'drop out of the contest', 
     }, 
-    reset: {
-        command: 'reset',
-        description: 'reset the contest', 
-    }, 
+    // reset: {
+    //     command: 'reset',
+    //     description: 'reset the contest', 
+    // }, 
     end: {
         command: 'end',
-        description: 'end the contest to choose a winner', 
+        description: 'end the contest to choose a winner *(please dont use this)*', 
     }, 
-    stats: {
-        command: 'stats',
-        description: 'show your fame contest stats', 
-    }, 
+    // stats: {
+    //     command: 'stats',
+    //     description: 'show your fame contest stats', 
+    // }, 
     help: {
         command: 'help',
         description: 'show help',
@@ -35,7 +35,7 @@ const contestEntriesKey = 'fameContestEntries';
 // Command Handling 
 
 function help(message, prefix) {
-    let description = 'enter daily for free fame! when the winner is announced, all the losers fame the winner.';
+    let description = '**this feature is in beta, don\'t expect it to work well**\nenter daily for free fame! when the contest ends, all the losers fame the winner.';
     let commandsString = 'commands:';
     for (const command in argumentType) {
         commandsString += '\n\t' + prefix + 'fame ' + argumentType[command].command + ' - ' + argumentType[command].description; 
@@ -111,6 +111,28 @@ function dropout(message) {
     }
 }
 
+function end(message) {
+    const entries = enmap.get(message.guild.id, contestEntriesKey);
+
+    if (entries.length >= 2) {
+        const winningNumber = Math.floor(Math.random() * entries.length); 
+        const winningEntry = entries[winningNumber];
+        const loserEntries = entries.filter(entry => entry.user !== winningEntry.user ); 
+
+        const winnerText = 'winner: <@' + winningEntry.user + '>'; 
+        const winnerCharText = 'winning character: `' + winningEntry.ign + '`';
+        const loserText = loserEntries.map(entry => '<@' + entry.user + '>').join(', ');
+
+        message.channel.send('the contest has ended. congratulations to the winner. losers, please fame the winner\'s character within 24 hours or suffer the consequences.\n\t' + winnerText + '\n\t' + winnerCharText + '\n\tlosers: ' + loserText);
+    }
+    else {
+        message.channel.send('boohoo not enough people entered. everyone loses.');
+    }
+
+    // reset all entries 
+    enmap.set(message.guild.id, [], contestEntriesKey);
+}
+
 // Model updates 
 
 function enterContest(guild, user, ign) {
@@ -147,25 +169,27 @@ module.exports = {
         console.log('Fame: ' + message.content);
         console.log(enmap.get(message.guild.id, contestEntriesKey));
 
+        // enmap.set(message.guild.id, [ { user: '0', ign: '0' }, { user: '1', ign: '1' }, { user: '2', ign: '2' }], contestEntriesKey);
+
         if (args[0] === argumentType.enter.command) {
             enter(message, args[1]);
         }
         else if (args[0] === argumentType.dropout.command) {
             dropout(message);
         }
-        else if (args[0] === argumentType.reset.command) {
-            message.channel.send('the contest has been reset');
-        }
+        // else if (args[0] === argumentType.reset.command) {
+        //     message.channel.send('the contest has been reset');
+        // }
         else if (args[0] === argumentType.end.command) {
-            message.channel.send('the contest has ended. the winner is');
+            end(message);
         }
-        else if (args[0] === argumentType.stats.command) {
-            message.reply('you have entered X times and won X times');
-        }
+        // else if (args[0] === argumentType.stats.command) {
+        //     message.reply('you have entered X times and won X times');
+        // }
         else if (args[0] === argumentType.help.command) {
             help(message, prefix);
         }
-        else if (args[0] === undefined) {
+        else if (args[0] === argumentType.view.command) {
             view(message);
         }
         else {
