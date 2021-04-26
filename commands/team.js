17168d6f54eb41d5e7eaeee1225d37dffd89570d
msmsandy <sanzoidy@gmail.com>
2021-04-26 13:37:21 -0400

    help embed

--- commands/team.js
+++ commands/team.js
@@ -17,72 +17,83 @@ const { view, list, viewType } = require('../src/team/team_view');
 const { mention } = require('../src/team/team_mention');
 const { checkin, checkout, clear } = require('../src/team/team_checkins');
 const { update, edit, remove } = require('../src/team/team_manage');
+const { getHelpEmbed } = require('../src/help/help_format');
 
 const argumentType = {
 	list: {
 		command: 'list', 
+		name: 'list teams',
 		arguments: '`teamid | here | all`',
 		description: 'show details of team(s)',
 	},
 	view: {
 		command: 'view', 
+		name: 'view teams',
 		arguments: '`teamid | here | all`',
 		description: 'show details of team(s) with checkins',
 	},
 	add: {
 		command: 'newteam', 
+		name: 'create new team',
 		arguments: '`teamid` `<# of slots>` `<channel>` \"`<name>`\" \"`<description>`\"',
 		description: 'add a new team with teamid, number of slots, channel (optional, default: current channel), name (optional), description (optional)',
 	}, 
 	delete: {
 		command: 'deleteteam',
+		name: 'delete team',
 		arguments: '`teamid`',
 		description: 'delete team with teamid',
 	},
 	edit: {
 		command: 'editteam',
+		name: 'edit team',
 		arguments: '`<setting: all | teamid | slots | channel | name | description>` `teamid` `value(s)`',
 		description: 'edit setting for team with teamid. `all` will overwrite all values',
 	}, 
 	checkin: {
 		command: 'checkin', 
+		name: 'checkin self',
 		arguments: '`teamid` `<optional: description>`', 
 		description: 'checkin to team with teamid. checking in again with a new description will update the description. only a max of 1 checkin per user can exist.',
 	}, 
 	checkinother: {
 		command: 'checkinother', 
+		name: 'checkin other',
 		arguments: '`teamid` `<optional: @user>` `<optional: description>`', 
 		description: 'checkin someone else to team with teamid. providing an @user will tie that user to the checkin for mentions. every checkinother will create a new checkin',
 	}, 
 	checkout: {
 		command: 'checkout',
+		name: 'checkout',
 		arguments: '`teamid | <# in list>`', 
 		description: 'checkout of team with teamid',
 	}, 
 	clear: {
 		command: 'clear',
+		name: 'clear team',
 		arguments: '`teamid | here`', 
 		description: 'clear team with teamid',
 	}, 
 	mention: {
 		command: 'mention', 
+		name: 'mention team',
 		arguments: '`teamid` `message`',
 		description: 'mention all users with message in the team with teamid',
 	}, 
 	help: {
 		command: 'help',
+		name: 'help',
 		arguments: '', 
 		description: 'view this',
 	},
 }
 
 function help(message, prefix) {
-    let description = '**team signups *(BETA)***\nplease let me know if there are any issues.\n';
-    let commandsString = 'commands:';
-    for (const command in argumentType) {
-        commandsString += `\n\t**${prefix}team ${argumentType[command].command} ${argumentType[command].arguments}**\n\t\t\t${argumentType[command].description}`;
-    }
-    message.channel.send(description + '\n' + commandsString);
+	let title = `\`${prefix}team\` - team signups`;
+	let description = `manage teams and allow members to signup`;
+
+	let embed = getHelpEmbed(argumentType, title, description);
+	message.channel.send(embed);
 }
 
 async function handleTeamDoesNotExist(message, teamId) {
